9562518d96083364ddc4716d88976443c68b70a3
dn200978lak <dn200978lak@gmail.com>
2015-06-30 10:37:58 +0300

    1.0-SNAPSHOT

--- addressbook.sql
+++ addressbook.sql
@@ -41,13 +41,14 @@ INSERT INTO `users` (`username`, `password`, `fio`, `telephone`, `email`, `addre
 --
 
 CREATE TABLE user_roles (
-  user_role_id INT(11) NOT NULL AUTO_INCREMENT,
-  username VARCHAR(30) NOT NULL,
-  ROLE VARCHAR(15) NOT NULL,
-  PRIMARY KEY (user_role_id),
-  UNIQUE KEY uni_username_role (ROLE,username),
-  KEY fk_username_idx (username),
-  CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES users (username));
+  `user_role_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(30) NOT NULL,
+  `ROLE` VARCHAR(15) NOT NULL,
+  PRIMARY KEY (`user_role_id`),
+  UNIQUE KEY uni_username_role (`ROLE`,`username`),
+  KEY fk_username_idx (`username`),
+  CONSTRAINT fk_username FOREIGN KEY (`username`) REFERENCES users (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
 
 --
 -- Дамп данных таблицы `user_roles`
